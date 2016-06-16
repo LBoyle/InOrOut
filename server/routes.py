1bfeae28e3846d2285ac36f9b6251f71a26d4474
LBoyle <louis_boyle@hotmail.com>
2016-06-16 12:51:34 +0100

    Fixed stuff, removed venv

--- server/routes.py
+++ server/routes.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify, send_file
+import datetime
+import myTweePy as twp
 
 app = Flask(__name__)
+print twp.handleJSON()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def homepage():
-	return render_template('index.html')
+	return render_template('leaflet.html', )
+
+@app.route('/deliver', methods=['GET', 'POST'])
+def overlay():
+	return send_file('static/data/geoFixed.json', mimetype="text/json")
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)

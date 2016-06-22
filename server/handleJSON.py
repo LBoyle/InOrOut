6e539111784fd67fe7eff444c2092c9385e8d681
LBoyle <louis_boyle@hotmail.com>
2016-06-22 22:49:01 +0100

    refined JSONhandler, simplified, actually send a string rgb value for each location

--- server/handleJSON.py
+++ server/handleJSON.py
@@ -16,11 +16,6 @@ def handleJSON():
 		timestamp = db.DateTimeField(default=datetime.datetime.now)
 		vCompound = db.FloatField()
 
-	# class LocData(db.Document):
-	#  	location = db.StringField()
-	#  	numPos = db.IntField()
-	#  	numNeg = db.IntField()
-
 	def logic():
 		resDict = {}
 		data = [(p.location, p.vCompound) for p in Tweet.objects]
@@ -33,39 +28,17 @@ def handleJSON():
 						countPos += 1
 					elif p[1] < -0.2:
 						countNeg +=1
-			
-			blue = (float(countPos)/float(countPos+countNeg))*255
-			red = (float(countNeg)/float(countPos+countNeg))*255
+			blue = 0
+			red = 0
+			if countPos > 1 or countNeg > 1:
+				blue = ((float(countPos)/float(countPos+countNeg))*255)
+				red = ((float(countNeg)/float(countPos+countNeg))*255)
 
-			resDict[i] = {"numPos" : countPos, "numNeg" : countNeg, "red":int(math.floor(red)), "blue":int(math.floor(blue))}
-		#	LocData(location=i, numPos=countPos, numNeg=countNeg).save()
-		# print resDict
+			resDict[i] = "rgb("+str(int(red))+",0,"+str(int(blue))+")"
+		
 		newJSON = open('static/data/procDict.json','r+')
 		newJSON.write(json.dumps(resDict))
 		newJSON.close()
 		
 	logic()
-
-
-# def handleJSON():
-# 	jsonOBJ = open('static/data/geoJSON.json', 'r')
-# 	Dict = json.loads(jsonOBJ.read())
-# 	jsonOBJ.close()
-# 	for i in Dict['features']:
-# 		#here's where the magic happens
-# 		i['properties']['colour'] = 'red'
-# 	newJSON = open('static/data/geoFixed.json','r+')
-# 	newJSON.write(json.dumps(Dict))
-# 	Dict = newJSON.read()
-# 	newJSON.close()
-
-# 	jsonOBJ = open('static/data/geoNIJSON.json', 'r')
-# 	Dict = json.loads(jsonOBJ.read())
-# 	jsonOBJ.close()
-# 	for i in Dict['features']:
-# 		#here's where the magic happens
-# 		i['properties']['colour'] = 'red'
-# 	newJSON = open('static/data/geoNIFixed.json','r+')
-# 	newJSON.write(json.dumps(Dict))
-# 	Dict = newJSON.read()
-# 	newJSON.close()
+# "rgb("+str(int(red))+",0,"+str(int(blue))+")"
